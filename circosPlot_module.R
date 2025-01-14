9f7f2b8c64da9add3bcb8b61adea877a177545f1
Lena <lenarallen19@gmail.com>
2025-01-14 10:31:24 -0800

    current progress generating multipleplots in shiny output

--- circosPlot_module.R
+++ circosPlot_module.R
@@ -100,7 +100,7 @@ circosPlotUI <- function(id, label = "Circos plot parameters"){
                  # output list, created in the "server" script"
                  fluidRow(
                    column(12, offset = 0, align = "left",
-                          plotOutput(ns("plot"), width = "600px")
+                          imageOutput(ns("plot"), width = "600px")
                    )))))))
   }
 
@@ -150,18 +150,12 @@ circosPlot <- function(input, output, session){
   ##                            CIRCOS                           --
   ##---------------------------------------------------------------
   
-  #subset dataframe to patients with the fusion selected in the dropdown
-
-  blank <- reactive({
-  pts_with_fusion <- reactive(patient_fusion_dt %>% filter(input$fusion_group == "yes") %>% rownames())
-  
-  circos_objects <- c()
-
-  #loop through all patients with fusion,
-  for (pt in pts_with_fusion) {
-
-    #get vcf data for that patient
-    vcf = svdata$pt
+  #function that plots circos plot for a given patient and saves it as a png (with a unique name)
+  #in the working directory 
+  circos_function <- function(pat) {
+      
+    #get vcf data for patient
+    vcf <- svdata[pat][[1]]
     
     #convert to BED
     vcf_bed <- vcf2bed(vcf, other = c("ID", "INFO", "ALT"))
@@ -237,7 +231,7 @@ circosPlot <- function(input, output, session){
     
     #MARKING CANONICAL FUSIONS
     #TARGET-20-PAVBIH-09A-02D_consensus -> MLLT10 gene on chromosome 10 with chromosome X
-    if (patient_name == "TARGET-20-PAVBIH-09A-02D"){
+    if (pat == "TARGET-20-PAVBIH-09A-02D"){
       bed_BND_CANON <- data.frame(
         chr = c("chr10", "chr10", "chr10"), 
         pos = c(21729167, 21729137, 21729166),
@@ -254,14 +248,14 @@ circosPlot <- function(input, output, session){
     
     #MARKING CANONICAL FUSIONS
     #TARGET-20-PAUZRY-09A-02D_consensus -> RUNX1 gene on chromosome 21 with RUNX1T1 gene on chromosome 8
-    if (patient_name == "TARGET-20-PAUZRY-09A-02D"){
+    if (pat == "TARGET-20-PAUZRY-09A-02D"){
       bed_BND_CANON <- data.frame("chr21", 34838337, 34838337, "RUNX1", 1)
       bed_BND_MATES_CANON <- data.frame("chr8", 92058671, 92058671, "RUNX1T1", 1)
     }
     
     #MARKING CANONICAL FUSIONS
     #TARGET-20-PAUNVN-09A-01D_Tumor_consensus -> ETV6 gene on chromosome 12 with chromosome 4 
-    if (patient_name == "TARGET-20-PAUNVN-09A-01D"){
+    if (pat == "TARGET-20-PAUNVN-09A-01D"){
       chr <- c("chr12", "chr12")
       chr_mate <- c("chr4", "chr4")
       pos <- c(117749121, 117749135)
@@ -275,14 +269,14 @@ circosPlot <- function(input, output, session){
     
     #MARKING CANONICAL FUSIONS
     #TARGET-20-PAURDN-03A-01D_consensus -> NUP98 gene on chromosome 11 with chromosome 5
-    if (patient_name == "TARGET-20-PAURDN-03A-01D"){
+    if (pat == "TARGET-20-PAURDN-03A-01D"){
       bed_BND_CANON <- data.frame("chr11", 3743985, 3743985, "NUP98", 1)
       bed_BND_MATES_CANON <- data.frame("chr5", 177233312, 177233312, " ", 1)
     }
     
     #MARKING CANONICAL FUSIONS
     #PAVESI-03A-01D_Tumor_consensus -> NUP98 gene on chromosome 11 with chromosome 5
-    if (patient_name == "PAVESI-03A-01D_Tumor"){
+    if (pat == "PAVESI-03A-01D_Tumor"){
       chr <- c("chr11", "chr11")
       chr_mate <- c("chr5", "chr5")
       pos <- c(3743776, 3743777)
@@ -296,7 +290,7 @@ circosPlot <- function(input, output, session){
     
     #MARKING CANONICAL FUSIONS
     #TARGET-20-PAUPIY-03A-01D_consensus -> NUP98 gene on chromosome 11 with chromosome 5
-    if (patient_name == "TARGET-20-PAUPIY-03A-01D"){
+    if (pat == "TARGET-20-PAUPIY-03A-01D"){
       chr <- c("chr11", "chr11", "chr11")
       chr_mate <- c("chr5", "chr5", "chr5")
       pos <- c(3739358, 3739361, 3680098)
@@ -308,8 +302,11 @@ circosPlot <- function(input, output, session){
       bed_BND_MATES_CANON <- data.frame(chr_mate, pos_mate, pos_mate, gene_mate, value)
     }
     
-    #make CIRCOS plots - need to save plot to a variable so it can be added to a list for plotting (in case of more than one patient)
+    #call png() for saving circlize output as a png
+    tempfilename <- paste(pat, ".png", sep = "")
+    png(filename = tempfilename)
     
+    #make CIRCOS plots--------------------------------------------------------------
     # INITIALIZE WITH IDEOGRAM (all chromosomes)
     circos.par(track.height = 0.08)
     circos.initializeWithIdeogram(species = "hg38")
@@ -335,27 +332,41 @@ circosPlot <- function(input, output, session){
     # CANONICAL TRANSLOCATIONS TRACK
     circos.genomicLink(bed_BND_CANON, bed_BND_MATES_CANON, col = "blue", border = "blue")
     
-    #add circos objects to list
     
-   
-      
-      #add circos objects to list
-      #?????????????????????
-      
-      }})
+    dev.off()
+    
   }
   
-  # if more than one patient, make all circos plots?
   
+  #reactive expression to get list of patients with the user-selected fusion
+  cp_objs <- reactive({
+  pts_with_fusion <- patient_fusion_dt %>% filter(input$fusion_group == "yes") %>% rownames()
+  pts_with_fusion <- as.list(pts_with_fusion)
+  return(pts_with_fusion)
+  })
   
-  # if only one patient, just plot
-  
-  
-  
-  
-  
-  
-  
-  
-# }
+
+  #Render circos plots 
+  output$plot <- renderPlot({
+    
+    #get list of patients with selected fusion
+    pts <- cp_objs()
+    
+    #if there are patients with the selected fusion (removing line part throws error, keeping it leads to no plots)
+    if(length(pts) != 0) {
+      
+      lapply(pts, circos_function)
+      png_filenames <- list.files(pattern="*.png")
+      rl <- lapply(png_filenames, png::readPNG)
+      gl <- lapply(rl, grid::rasterGrob)
+      grid.arrange(grobs=gl)
+      
+      
+    }
+  })
+    
+  #end of server function
+  }
+
+
   
